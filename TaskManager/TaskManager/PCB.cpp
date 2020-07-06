2ab6bb688c46bc786f3b153edb299ea3265e9ad1
collaralloc <collar@126.com>
2020-07-06 22:20:51 +0800

    Changes to be committed: 	modified:   TaskManager/TaskManager/PCB.cpp 	modified:   TaskManager/TaskManager/PCB.h 	new file:   TaskManager/TaskManager/PriorityTask.cpp

--- TaskManager/TaskManager/PCB.cpp
+++ TaskManager/TaskManager/PCB.cpp
@@ -16,3 +16,29 @@ void PCB::show()
 {
 	printf("PCB show\n");
 }
+Plist::Plist() {
+	pc = 0;
+	length = 0;
+	pcb = NULL;
+}
+PCB* Plist::create() {
+	PCB *p = new PCB();
+	p->pid = ++pc;
+	p->prio = rand()%100;
+	p->round = rand();
+	p->need_time = rand()%500;
+	p->cpu_time = 0;
+	p->status = 0;
+	p->next = NULL;
+	return p;
+}
+void Plist::insertP(PCB* p) {
+	length++;
+	if (pcb == NULL) {
+		pcb = p;
+		return;
+	}
+
+	while (pcb->next != NULL) {
+	}
+}
--- TaskManager/TaskManager/PCB.h
+++ TaskManager/TaskManager/PCB.h
@@ -2,8 +2,30 @@
 class PCB
 {
 public:
+	int pid;	//进程标识符
+	int prio;	//进程优先数
+	int round;	//进程轮转时间片
+	int cpu_time;//进程占用CPU时间
+	int count;	//计数器
+	int need_time;	//进程到完成还需要的CPU时间
+	int status;	//状态
+	PCB* next;
+
+
 	PCB();
 	~PCB();
 	void show();
-};
+	
 
+};
+class Plist {
+	PCB* pcb;	//队首
+	int pc;		//自增编号
+	int length;	//进程数
+	Plist();
+	void insertP(PCB* p);
+	void insertR(PCB* p);
+	void firstin();
+	void print();
+	PCB* create();
+};
